b1bc9729c3dcf5362925cc265dfa6600a1eabf7f
Diego F <dev6.seucondominio@gmail.com>
2017-06-22 13:45:38 -0300

    Cruds: C++: Ajustando main (OK)

--- cruds/pessoas/c++/main.cpp
+++ cruds/pessoas/c++/main.cpp
@@ -6,68 +6,81 @@
 #include "menu_pessoa.h"
 
 #include <vector>
-#include <iostream>
-#include <typeinfo>
 
 using namespace std;
 
-int main(int argc, char **argv) {
-  Pessoa pessoa;
+// functions
+int get_code_options() {
+  int code;
   MenuPessoa menu_pessoa;
-
   vector<string> items = menu_pessoa.all();
 
-  int code;
-  while (true) {
-    system("clear");
+  system("clear");
 
-    cout << " Escolha uma das opções: \n\n";
-    for(vector<string>::const_iterator i = items.begin(); i != items.end(); ++i) {
-      cout << ' ' << *i << endl;
+  cout << " Escolha uma das opções: \n\n";
+  for(vector<string>::const_iterator i = items.begin(); i != items.end(); ++i) {
+    cout << ' ' << *i << endl;
+  }
+  cout << endl << " Opção: ";
+
+  cin >> code;
+  system("clear");
+
+  return code;
+}
+
+void list_people() {
+  system("clear");
+
+  cout << "\n Lista de Pessoas \n\n";
+
+  Pessoa pessoa;
+  vector<string> pessoas = pessoa.all();
+
+  if (pessoas.empty()) {
+    cout << "   Nenhuma pessoa encontrada \n\n";
+  } else {
+    for(vector<string>::const_iterator i = pessoas.begin(); i != pessoas.end(); ++i){
+      cout << " ----- Pessoa " << (i - pessoas.begin()) + 1 << "\n";
+      cout << "  Nome: " << *i << "\n\n";
     }
-    cout << endl << " Opção: ";
+  }
 
-    cin >> code;
+  cout << " Digite enter para continuar";
+  cin.get();
+  cin.get();
+}
+
+void create_person() {
+  Pessoa pessoa;
+
+  string nome = "";
+  while (nome.empty()) {
     system("clear");
+    cout << "\n Cadastro de Pessoa (0 para cancelar) \n\n";
+    cout << " Nome: ";
+    getline(cin, nome);
+  }
+
+  if (nome != "0") {
+    pessoa.create(nome);
+  }
+}
+
+// main function
+int main(int argc, char **argv) {
+  int code;
+
+  while (true) {
+    code = get_code_options();
 
     switch (code) {
       case 0: {
         cout << "\n Obrigado! \n\n";
         return 0;
       }
-      case 1: {
-        cout << "\n Lista de Pessoas \n\n";
-
-        vector<string> pessoas = pessoa.all();
-
-        if (pessoas.empty()) {
-          cout << "   Nenhuma pessoa encontrada \n\n";
-        } else {
-          for(vector<string>::const_iterator i = pessoas.begin(); i != pessoas.end(); ++i){
-            cout << " ----- Pessoa " << (i - pessoas.begin()) + 1 << "\n";
-            cout << "  Nome: " << *i << "\n\n";
-          }
-        }
-
-        cout << " Digite enter para continuar";
-        cin.get();
-        cin.get();
-        break;
-      }
-      case 2: {
-        string nome = "";
-        while (nome.empty()) {
-          system("clear");
-          cout << "\n Cadastro de Pessoa (0 para cancelar) \n\n";
-          cout << " Nome: ";
-          getline(cin, nome);
-        }
-
-        if (nome != "0") {
-          pessoa.create(nome);
-        }
-        break;
-      }
+      case 1: { list_people(); break; }
+      case 2: { create_person(); break; }
     }
   }
 }
--- cruds/pessoas/ruby/main.rb
+++ cruds/pessoas/ruby/main.rb
@@ -5,49 +5,66 @@
 load 'pessoa.rb'
 load 'menu_pessoa.rb'
 
-items = MenuPessoa.all
-
 while true
+  code = get_code_options
+
+  case code
+  when '0'
+    puts "\n Obrigado! \n\n"
+    return
+  when '1' then list_people
+  when '2' then create_person
+  end
+end
+
+def get_code_options
   system("clear")
 
   puts " Escolha uma das opções: \n\n"
-  items.each{ |item| puts " #{item['code']} - #{item['name']}" }
+  MenuPessoa.all.each{ |item| puts " #{item['code']} - #{item['name']}" }
   puts "\n"
 
   print " Opção: "
   code = gets.strip
   system("clear")
 
-  case code
-  when '0'
-    puts "\n Obrigado! \n\n"
-    exit
-  when '1'
-    pessoas = Pessoa.all
+  code
+end
 
-    puts "\n Lista de Pessoas \n\n"
-    if pessoas.empty?
-      puts "   Nenhuma pessoa encontrada \n\n"
-    else
-      pessoas.each_with_index do |pessoa, idx|
-        puts " ----- Pessoa #{idx + 1}\n"
-        puts "  Nome: #{pessoa['name']}"
-        puts "\n"
-      end
+def list_people
+  pessoas = Pessoa.all
+
+  system("clear")
+  puts "\n Lista de Pessoas \n\n"
+  if pessoas.empty?
+    puts "   Nenhuma pessoa encontrada \n\n"
+  else
+    pessoas.each_with_index do |pessoa, idx|
+      puts " ----- Pessoa #{idx + 1}\n"
+
+      text  = ["  Nome: #{pessoa['name']}"]
+      text << "Age: #{pessoa['age']}" if pessoa['age']
+
+      puts text.join(', ')
+      puts "\n"
     end
-
-    print " Digite enter para continuar"
-    gets.strip
-  when '2'
-    nome = ''
-    while nome.empty?
-      system("clear")
-      puts "\n Cadastro de Pessoa (0 para cancelar) \n\n"
-
-      print " Nome: "
-      nome = gets.strip
-    end
-
-    Pessoa.create(name: nome) unless nome == '0'
   end
+
+  print " Digite enter para continuar"
+  gets.strip
+end
+
+def create_person
+  nome = ''
+  while nome.empty?
+    system("clear")
+    puts "\n Cadastro de Pessoa (0 para cancelar) \n\n"
+
+    print " Nome: "
+    nome = gets.strip
+  end
+
+  Pessoa.create(name: nome) unless nome == '0'
+
+  list_people
 end
